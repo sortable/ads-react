81fbcba5fa0133299a5bb4a74bbbd2d1e3faffa9
yunsong <yunsong@sortable.com>
2017-12-07 15:01:53 -0500

    Add TimeRefreshAd

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,13 @@ export interface AdProps {
   id: string;
 }
 
-export default class Ad extends React.Component<AdProps, any> {
+/**
+ * Define and display ad
+ *
+ * Usage:
+ *   <Ad id="div-id-1" />
+ */
+export class Ad extends React.Component<AdProps, any> {
   public componentDidMount() {
     adsManager.requestAds([this.props.id]);
   }
@@ -27,5 +33,65 @@ export default class Ad extends React.Component<AdProps, any> {
 
   public render() {
     return React.createElement('div', { id: this.props.id });
+  }
+}
+
+export interface TimeRefreshAdProps extends AdProps {
+  interval: number;
+}
+
+/**
+ * Refresh ad by interval
+ *
+ * Usage:
+ *   <TimeRefreshAd id="div-id-1" interval={30} />
+ *
+ * ^ Above example will refresh the ad every 30s automatically.
+ */
+export class TimeRefreshAd extends React.Component<TimeRefreshAdProps, any> {
+  private timer: number|null;
+
+  constructor(props: TimeRefreshAdProps) {
+    super(props);
+
+    this.timer = null;
+  }
+
+  public componentDidMount() {
+    this.resetTimer();
+  }
+
+  public shouldComponentUpdate(nextProps: TimeRefreshAdProps) {
+    return this.props.id !== nextProps.id || this.props.interval !== nextProps.interval;
+  }
+
+  public componentWillUpdate(nextProps: TimeRefreshAdProps) {
+    this.clearTimer();
+  }
+
+  public componentDidUpdate() {
+    this.resetTimer();
+  }
+
+  public componentWillUnmount() {
+    this.clearTimer();
+  }
+
+  public render() {
+    return React.createElement(Ad, { id: this.props.id });
+  }
+
+  private clearTimer() {
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+    }
+  }
+
+  private resetTimer() {
+    this.clearTimer();
+    this.timer = setInterval(() => {
+      // call requestAds again to refresh it
+      adsManager.requestAds([this.props.id]);
+    }, this.props.interval * 1000);
   }
 }
