4f6bb0ee804865d632ad74546909b8a1ca1a5850
gyson <eilian.yunsong@gmail.com>
2017-12-09 20:08:15 -0500

    PR feedback

---
+++ src/ad.ts
@@ -0,0 +1,43 @@
+import * as adsManager from '@sortable/ads';
+import * as React from 'react';
+
+export interface AdProps {
+  id: string;
+  refreshKey?: string;
+}
+
+/**
+ * Define and display ad
+ *
+ * Usage:
+ *   <Ad id="div-id-1" />
+ */
+export class Ad extends React.Component<AdProps, any> {
+  public componentDidMount() {
+    adsManager.requestAds([this.props.id]);
+  }
+
+  public shouldComponentUpdate(nextProps: AdProps) {
+    return this.props.id !== nextProps.id || this.props.refreshKey !== nextProps.refreshKey;
+  }
+
+  public componentWillUpdate(nextProps: AdProps) {
+    if (this.props.id !== nextProps.id) {
+      adsManager.destroyAds([this.props.id]);
+    }
+  }
+
+  public componentDidUpdate(prevProps: AdProps) {
+    if (this.props.id !== prevProps.id || this.props.refreshKey !== prevProps.refreshKey) {
+      adsManager.requestAds([this.props.id]);
+    }
+  }
+
+  public componentWillUnmount() {
+    adsManager.destroyAds([this.props.id]);
+  }
+
+  public render() {
+    return React.createElement('div', { id: this.props.id });
+  }
+}
